466e73296af5179cd52c6371cd2f094518cb59df
giorgiameffe <giorgiameffe03@gmail.com>
2025-05-13 00:35:53 +0200

    feat: add search filter

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -2,15 +2,19 @@ import { Link } from "react-router-dom";
 
 function MovieCard({ data }) {
 
-    const { id, title, abstract, imagePath, director } = data;
+    const { id, title, abstract, imagePath, director, reviews_vote } = data;
+
+    const reviewsVote = parseFloat(reviews_vote)
+    // console.log(reviewsVote);
 
     return (
         <article className="card">
-            <img src={imagePath} alt={title} className="w-50 mx-auto" />
+            <img src={imagePath} alt={title} className="w-50 h-50 mx-auto img-fluid rounded" />
             <div className="card-body text-center">
                 <h5 className="card-title">{title}</h5>
                 <p>Director: <strong>{director}</strong></p>
-                <p className="card-text">{abstract}</p>
+                <p>Reviews: {reviewsVote}</p>
+                <p className="card-text fst-italic">{abstract}</p>
                 <Link to={`/movies/${id}`} className="btn btn-primary">Get more info</Link>
             </div>
         </article>
--- pages/Movies.jsx
+++ pages/Movies.jsx
@@ -7,15 +7,28 @@ function MoviesPage() {
 
     const [movies, setMovies] = useState([]);
 
+    const [search, setSearch] = useState('');
+
+
     function getMovies() {
 
         const url = 'http://127.0.0.1:3000/movies';
 
-        axios.get(url)
+        axios.get(url, {
+            params: {
+                search
+            }
+        })
             .then(response => {
                 setMovies(response.data)
             })
             .catch(err => console.log(err))
+    }
+
+    function searchMovies(e) {
+
+        e.preventDefault();
+        getMovies();
     }
 
     useEffect(getMovies, [])
@@ -24,6 +37,19 @@ function MoviesPage() {
         <>
             <div className="d-flex justify-content-between align-items-center">
                 <h1 className="mb-4">Movies</h1>
+                <form onSubmit={searchMovies} className="row g-3">
+                    <div className="col-auto">
+                        <label className="visually-hidden">Search</label>
+                        <input type="text" className="form-control font-bold" placeholder="Search movie"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                    </div>
+                    <div className="col-auto">
+                        <button type="submit" className="btn btn-primary mb-3">Search movie</button>
+                    </div>
+                </form>
+
             </div>
 
             <section className="row gy-3">
