5228309ebdb0cf54dbdc29eace448e6679b11900
giorgiameffe <giorgiameffe03@gmail.com>
2025-05-12 16:28:35 +0200

    feat: create movies and movie details routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <h1>Webapp React</h1>
-    </>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>Homepage</div>} />
+        <Route path='/movies' element={<div>Movies</div>} />
+        <Route path='/movies/:id' element={<div>Movie Details</div>} />
+        <Route path='*' element={<div>404 Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
