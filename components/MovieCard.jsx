cc5f568d38235e35ed4fa62472ee02e07bb0f37f
giorgiameffe <giorgiameffe03@gmail.com>
2025-10-17 12:24:28 +0200

    feat: add background img for movies page and style

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -6,14 +6,17 @@ function MovieCard({ data }) {
     const { id, title, abstract, imagePath, director, reviews_vote, slug } = data;
 
     return (
-        <article className="card container movie-card">
-            <img src={imagePath} alt={title} className="w-50 mx-auto rounded" />
-            <div className="card-body text-center">
+
+        <article className="card container movie-card h-100 d-flex flex-column">
+            <img src={imagePath} alt={title} className="w-50 mx-auto mt-3 rounded" />
+            <div className="card-body text-center d-flex flex-column flex-grow-1">
                 <h5 className="card-title">{title}</h5>
                 <p>Director: <strong>{director}</strong></p>
                 <p>Average rating: <StarRating vote={reviews_vote} /></p>
-                <p className="card-text fst-italic">{abstract}</p>
-                <Link to={`/movies/${slug}`} className="btn btn-dark text-white">Get more info</Link>
+                <p className="card-text fst-italic flex-grow-1">{abstract}</p>
+                <div className="mt-auto">
+                    <Link to={`/movies/${slug}`} className="btn btn-movies p-2 text-dark">Get more info</Link>
+                </div>
             </div>
         </article>
     )
--- pages/Movies.jsx
+++ pages/Movies.jsx
@@ -1,72 +1,56 @@
 import MovieCard from "../components/MovieCard";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
 import { useContext } from "react";
 import GlobalContext from "../contexts/GlobalContext";
 
-
 function MoviesPage() {
-
     const [movies, setMovies] = useState([]);
-
     const [search, setSearch] = useState('');
-
     const { setIsLoading } = useContext(GlobalContext);
 
-
     function getMovies() {
-
         setIsLoading(true);
-        const url = 'http://127.0.0.1:3000/movies';
-
-        axios.get(url, {
-            params: {
-                search
-            }
-        })
-            .then(response => {
-                setMovies(response.data)
-            })
+        axios.get('http://127.0.0.1:3000/movies', { params: { search } })
+            .then(res => setMovies(res.data))
             .catch(err => console.log(err))
-            .finally(() => setIsLoading(false))
+            .finally(() => setIsLoading(false));
     }
 
     function searchMovies(e) {
-
         e.preventDefault();
         getMovies();
     }
 
-    useEffect(getMovies, [])
+    useEffect(getMovies, []);
 
     return (
-        <>
-            <div className="d-flex justify-content-between align-items-center">
-                <h1 className="mb-4">Movies</h1>
-                <form onSubmit={searchMovies} className="row g-3">
-                    <div className="col-auto">
-                        <label className="visually-hidden">Search</label>
-                        <input type="text" className="form-control font-bold" placeholder="Search movie"
+        <div className="movies-page">
+            <div className="container position-relative py-5 content-wrapper">
+                <form onSubmit={searchMovies} className="d-flex justify-content-center mb-5 search-container">
+                    <div className="d-flex search-bar-wrapper">
+                        <input
+                            type="text"
+                            className="form-control movie-search-input p-2 pe-3 ps-3 me-3" // Nuova classe
+                            placeholder="Write your movie"
                             value={search}
-                            onChange={(e) => setSearch(e.target.value)}
+                            onChange={e => setSearch(e.target.value)}
                         />
-                    </div>
-                    <div className="col-auto">
-                        <button type="submit" className="btn btn-dark mb-3">Search movie</button>
+                        <button type="submit" className="btn btn-movies p-2 pe-3 ps-3">Search</button>
                     </div>
                 </form>
 
-            </div>
+                <section className="row g-3">
+                    {movies.length ? movies.map(movie => (
+                        <div className="col-md-4 d-flex aling-items-stretch" key={movie.id}>
+                            <MovieCard data={movie} />
+                        </div>
+                    )) : <div className="text-white">No items found</div>}
+                </section>
 
-            <section className="row gy-3">
-                {movies.length ? movies.map(movie =>
-                    <div className="col-lg-4 col-md-6 col-sm-12" key={movie.id}>
-                        <MovieCard data={movie} />
-                    </div>) : <div>No items found</div>}
-            </section>
-        </>
-    )
+            </div>
+        </div>
+    );
 }
 
 export default MoviesPage;
