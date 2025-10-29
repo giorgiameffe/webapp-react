aa124c20c01aafefba7375f4e35eeb9647ffb6d3
giorgiameffe <giorgiameffe03@gmail.com>
2025-10-29 23:22:24 +0100

    feat: create not found page

--- components/NotFound.jsx
+++ components/NotFound.jsx
@@ -7,7 +7,7 @@ function NotFound() {
             <div className="not-found-container fs-5 d-flex align-items-center justify-content-center flex-column text-white p-4 rounded-4">
                 <h3 className="fw-bold">404 Not Found</h3>
                 <p className="pb-2 fst-italic">Sorry but the page you are looking for is not avalaible.</p>
-                <Link to={'/movies'} className="btn btn-movies p-2 pe-4 ps-4 text-dark">Click here</Link>
+                <Link to={'/movies'} className="btn btn-movies p-2 pe-4 ps-4 text-dark">Turn back to movies</Link>
             </div>
         </div>
     )
---
+++ pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import NotFound from "../components/NotFound"
+
+function NotFoundPage() {
+
+    return (
+        <>
+            <NotFound />
+        </>
+    )
+}
+
+export default NotFoundPage;
